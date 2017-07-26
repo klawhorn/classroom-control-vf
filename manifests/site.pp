ef7056b18198ad21af83c5aa813889ebf862d2a9
laxmikanth4 <laxmikanth304@gmail.com>
2017-07-26 15:52:23 -0500

    adding nginx

--- manifests/site.pp
+++ manifests/site.pp
@@ -45,6 +45,7 @@ node default {
   include role::classroom
   include users
   include skeleton
+  include nginx
   exec {'cowsay motd':
   command     => "cowsay 'Welcome to ${::fqdn}!' > /etc/motd",  # (namevar) The actual command to execute.  Must either be...
   path        => '/usr/local/bin',
