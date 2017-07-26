d7d4f4f47e0b2ba169abbef46afb883a1d0479d0
Katie Lawhorn <katie.lawhorn@puppet.com>
2017-07-26 13:10:29 -0700

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,3 +1,15 @@
 class skeleton {
-
+  file { '/etc/skel' :
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+  }
+  
+  file { 'bashrc' :
+    ensure => file,
+    path => 'etc/skel/.bashrc',
+    owner => 'root',
+    group => 'root',
+    source => 'puppet:///modules/skeleton/bashrc',
+  }
 }
