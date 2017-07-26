1863153ccde4062f8816b0b7c5252ce49c381f8a
Tara Swenson <kavi.xiu@gmail.com>
2017-07-26 15:40:26 -0700

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,3 +1,17 @@
 class memchached {
+  package {'memcached':
+    ensure  =>  present,
+    before  =>  File
+  }
+file { 'memcached':
+  ensure  =>  file,
+  path  =>  '/etc/sysconfig/memcached',
+  owner =>  'root',
+  group =>  'root',
+  source  =>  'puppet:///modules/memcached/memcached',
+}
 
+service { 'memcached':
+  ensure  =>  running,
+  enable  =>  true,
 }
