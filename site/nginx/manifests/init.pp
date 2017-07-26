53750c10edb971f6167d1112d4afe710200f7505
Katie Lawhorn <katie.lawhorn@puppet.com>
2017-07-26 15:42:13 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,6 +1,7 @@
 class nginx {
 
   File {
+    ensure => file,
     owner => 'root',
     group => 'root',
   }
@@ -20,19 +21,16 @@ class nginx {
   }
   
   file { 'index.html':
-    ensure => file,
     path   => '/var/www/index.html',
     source => 'puppet:///modules/nginx/index.html',
   }
   
   file { 'nginx.conf':
-    ensure => file,
     path   => '/etc/nginx/nginx.conf',
     source => 'puppet:///modules/nginx/nginx.conf',
   }
   
   file { 'default.conf':
-    ensure => file,
     path   => '/etc/nginx/conf.d/default.conf',
     source => 'puppet:///modules/nginx/default.conf',
   }
