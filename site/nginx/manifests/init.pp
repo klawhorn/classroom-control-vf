cb2f458a113143d5db77f0f49e36d4cc1140e7c5
Katie Lawhorn <katie.lawhorn@puppet.com>
2017-07-26 16:01:44 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,16 +1,11 @@
 class nginx {
 
-  $docroot = '/var/www'
-  $confdir = '/etc/nginx'
-  $uri = 'puppet:///modules/nginx'
-
-  File {
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-  }
+  $docroot  = '/var/www'
+  $confdir  = '/etc/nginx'
+  $blockdir = "/etc/nginx/conf.d"
+  $uri      = 'puppet:///modules/nginx'
   
-  package { 'nginx':
+  package { 'nginx' :
     ensure => present,
     before => [ 
       File['nginx.conf'],
@@ -18,24 +13,30 @@ class nginx {
       File['index.html']
     ]
   }
-
+  
+  File {
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+  }
+  
   file { $docroot :
     ensure => directory,
   }
   
   file { 'index.html' :
-    path   => '${docroot}/index.html',
-    source => '${uri}/index.html',
+    path   => "${docroot}/index.html",
+    source => "${uri}/index.html",
   }
   
   file { 'nginx.conf' :
-    path   => '${confdir}/nginx.conf',
-    source => '${uri}/nginx.conf',
+    path   => "${confdir}/nginx.conf",
+    source => "${uri}/nginx.conf",
   }
   
   file { 'default.conf' :
-    path   => '${confdir}/conf.d/default.conf',
-    source => '${uri}/default.conf',
+    path   => "${blockdir}/default.conf",
+    source => "${uri}/default.conf",
   }
   
   service { 'nginx' :
