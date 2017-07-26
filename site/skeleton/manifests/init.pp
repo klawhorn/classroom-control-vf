a99cf55899f2b84c5766f1945433e7b5733948be
Tara Swenson <kavi.xiu@gmail.com>
2017-07-26 13:13:01 -0700

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,1 +1,16 @@
+class skeleton {
+  file { '/etc/skel':
+    ensure  =>  directory,
+    owner   =>  'root',
+    group   =>  'root,
+  }
 
+  file {'bashrc':
+    ensure  =>  file,
+    path    =>  '/etc/skel/.bashrc',
+    owner   =>  'root',
+    group   =>  'root',
+    source  =>  'puppet:///modules/skeleton/bashrc',
+  }
+  
+}
