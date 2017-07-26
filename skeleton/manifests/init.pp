4510800a036ccd0fae068cbdafc2b071202b89a7
laxmikanth4 <lakshmikanth304@gmail.com>
2017-07-26 15:12:57 -0500

    Update init.pp

--- skeleton/manifests/init.pp
+++ skeleton/manifests/init.pp
@@ -1,4 +1,17 @@
 class skeleton {
 
+  file { '/etc/skel':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+  }
+
+  file { 'bashrc':
+    ensure => file,
+    path   => '/etc/skel/.bashrc'
+    owner  => 'root',
+    group  => 'root',
+    source => 'puppet:///modules/skeleton/bashrc'
+  }
 
 }
