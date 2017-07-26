6956ddf08c3c34b5316744a64c24b8d66edb5af4
jaramer <jaramer@gmail.com>
2017-07-26 09:55:18 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,7 +43,7 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
-  }
+  
   file { '/etc/motd':
   ensure  => file,
   owner   => 'root',
@@ -51,5 +51,5 @@ node default {
   mode    => '0644',
   content => "Think before you type\n",
 
-
+}
 }
