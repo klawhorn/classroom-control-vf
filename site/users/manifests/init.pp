f493e007d29cd1e4e4480d9da10aa6db09a09941
laxmikanth4 <lakshmikanth304@gmail.com>
2017-07-26 13:43:26 -0500

    Update init.pp

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -2,11 +2,9 @@
 class users {
   user { 'fundamentals':
     ensure           => 'present',
-    gid              => '1002',
     home             => '/home/fundamentals',
     password_max_age => '99999',
     password_min_age => '0',
     shell            => '/bin/bash',
-    uid              => '1002',
   }
 }
