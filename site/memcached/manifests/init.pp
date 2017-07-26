aed21560c0d87d7d213b70e3d42c67bc5ce262d3
Katie Lawhorn <katie.lawhorn@puppet.com>
2017-07-26 14:57:34 -0700

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,3 +1,21 @@
-class {
-
+class memcached {
+  package { 'memcached' :
+    ensure => present,
+    before => File['memcached'],
+  }
+  
+  file { 'memcached' :
+    ensure => file,
+    path => '/etc/sysconfig/memcached',
+    owner => 'root',
+    group => 'root',
+    source => 'puppet:///modules/memcached/memcached',
+    notify => Service['memcached'],
+  }
+  
+  service { 'memcached' :
+    ensure => running,
+    enable => true,
+  }
+  
 }
