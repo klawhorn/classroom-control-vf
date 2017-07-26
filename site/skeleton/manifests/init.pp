302942a55312f45dfc55aed6aaa71cebfe07c4d1
jaramer <jaramer@gmail.com>
2017-07-26 13:11:32 -0700

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,1 +1,15 @@
-   ..
+class skeleton {
+file {'/etc/skel':
+   ensure => directory
+   owner =>'root',
+   group => 'root',
+}
+
+file {batchrc':
+ensure => file,
+path => 'etc/skel/.bashrc',
+owner =>'root',
+group => 'root',
+source => 'puppet:///modules/skeleton/
+}
+}
