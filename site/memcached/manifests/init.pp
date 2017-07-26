5df87ca0df14b6ac69e0a9db28c6e07f7b62330c
laxmikanth4 <lakshmikanth304@gmail.com>
2017-07-26 17:06:58 -0500

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,14 +1,14 @@
 class memcached {
   package { 'memcached':
     ensure => present,
-    before => File ['memcached'],
+    before => File['memcached'],
   }
 
   file {'memcached':
     ensure => file,
     path   => '/etc/sysconfig/memcached' 
     source => 'puppet:///modules/memcached/memcached'
-    notify => Service [memcached],
+    notify => Service[memcached],
   } 
 
   service {'memcached':
