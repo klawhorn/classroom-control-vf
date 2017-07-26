dc862b685406759c48e985179f5b1a75345a1372
jaramer <jaramer@gmail.com>
2017-07-26 10:52:03 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,11 +44,17 @@ node default {
   #   class { 'my_class': }
   include role::classroom
   
-  file { '/etc/motd':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => "Think before you type brackets make a difference\n",
+  #file { '/etc/motd':
+  #ensure  => file,
+  #owner   => 'root',
+  #group   => 'root',
+  #mode    => '0644',
+  #content => "Think before you type brackets make a difference\n",
+  
+  exec {'motd':
+  command => "cowsay 'Welcome to ${fqdn}' > /etc/motd",
+  path => /usr/local/bin',
+  creates => '/etc/motd',
+  }
 }
 }
