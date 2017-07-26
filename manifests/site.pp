fadb2b05aefec9d020c3e2dc78e4b2148dc27cad
jaramer <jaramer@gmail.com>
2017-07-26 10:03:09 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -49,6 +49,6 @@ node default {
   owner   => 'root',
   group   => 'root',
   mode    => '0644',
-  content => "Think before you type\n",
+  content => "Think before you type brackets make a difference\n",
 }
 }
