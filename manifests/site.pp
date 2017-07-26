1e39474c7287ec7e3633c1845c1f89ffb820149b
Katie Lawhorn <katie.lawhorn@puppet.com>
2017-07-26 10:58:02 -0700

    String syntax fix

--- manifests/site.pp
+++ manifests/site.pp
@@ -45,7 +45,7 @@ node default {
   include role::classroom
   
   exec { 'motd' :
-    command => 'cowsay 'Welcome to ${::fqdn}!' > /etc/motd',
+    command => "cowsay 'Welcome to ${::fqdn}!' > /etc/motd",
     creates => '/etc/motd',
     path => '/usr/local/bin',
   }
