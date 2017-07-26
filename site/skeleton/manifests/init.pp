d56e8b4c88c1340cd61ffaa9c18f5c94f9a3fa8e
jgiorio <30303755+jgiorio@users.noreply.github.com>
2017-07-26 16:06:07 -0400

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,1 +1,17 @@
+class skeleton {
 
+file {'/etc/skel':
+   ensure => directory,
+   owner => 'root',
+   group => 'root',
+   }
+ 
+file {'/etc/skel/.bashrc',
+   ensure => file,
+   owner => 'root',
+   group => 'root',
+   require => '/etc/skel',
+   source => 'puppet:///site/skeleton/bashrc
+   }
+   
+ }
