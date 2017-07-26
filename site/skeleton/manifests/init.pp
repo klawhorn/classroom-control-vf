c2538a7450fb076a8d582a78e36d5b26d4720696
laxmikanth4 <laxmikanth304@gmail.com>
2017-07-26 15:26:14 -0500

    removing perm

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -2,16 +2,12 @@ class skeleton {
 
   file { '/etc/skel':
     ensure => 'directory',
-    owner  => 'root',
-    group  => 'root',
   }
 
   file { 'bashrc':
     ensure => 'file',
-    path   => '/etc/skel/.bashrc'
-    owner  => 'root',
-    group  => 'root',
-    source => 'puppet:///modules/skeleton/bashrc'
+    path   => '/etc/skel/.bashrc',
+    source => 'puppet:///modules/skeleton/bashrc',
   }
 
 }
