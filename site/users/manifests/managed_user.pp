a021001664d395f72239911670c02eb59303febd
Robert Tate <rob@rtate.se>
2017-07-27 14:06:29 -0400

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,4 +1,14 @@
 define users::managed_user (
-  $group = $title,
+  $group = $title
 ) {
+  $home = $facts['os']['family'] ? {
+    'windows' => 'C:/Users',
+    default   => '/home'
+  }
+  user { $title:
+    ensure => present
+  }
+  file { "/home/$title":
+    ensure => directory
+  }
 }
