a7afa3ed49ae2571fce6f2394f2c118786a3bdf5
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-07-27 12:03:39 -0600

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,5 +1,25 @@
-define users::managed_user {
+define users::managed_user (
+  $group = $title,
+){
+  user { $title:
+    ensure => present,
+  }
+  
+  $home = $facts['os']['family'] ? {
+    'windows' => 'C:/Users',
+    default   => '/home',
+  }
 
-
-
+  file { "${home}/${title}":
+    ensure => directory,
+    owner  => $title,
+    group  => $title,
+  }
+  
+  file { "${home}/${title}/.ssh":
+    ensure => directory,
+    owner  => $title,
+    group  => $title,
+    mode   => '0700',
+  }
 }
