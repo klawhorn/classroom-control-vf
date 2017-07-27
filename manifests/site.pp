2b5309093cf4ef4bbe7d7e1e4b192a3f01d686a6
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-07-27 14:40:02 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,9 +38,11 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-#node josephoaks.puppetlabs.vm {
-#  include skeleton
-#}
+node josephoaks.puppetlabs.vm {
+  class { 'nginx':
+    root => '/var/www/sloop',
+  }
+}
 
 node default {
   # This is where you can declare classes for all nodes.
