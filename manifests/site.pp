33ee44e74190a90acd56f0772b6b8b77684235b6
Michael Kelly <kellymf@praecipio.com>
2017-07-27 11:20:47 -0500

    playing more with lab 12.2

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,8 +42,8 @@ node mfkellytx.puppetlabs.vm {
 # This is where you can declare classes for all nodes. # Example:
 # class { 'my_class': }
 if $::virtual != 'physical' {
-$vmname = capitalize($::virtual)
-notify { "This is a ${vmname} virtual machine.": }
+$vmname = capitalize($facts['virtual'])
+notify { "$facts['hostname'] is a ${vmname} virtual machine.": }
 } }
 
 node default {
