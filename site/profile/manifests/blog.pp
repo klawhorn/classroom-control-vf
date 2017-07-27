a2f043088bc66996477f5dd91274c98335ca2271
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-07-27 16:57:52 -0600

    Update blog.pp

--- site/profile/manifests/blog.pp
+++ site/profile/manifests/blog.pp
@@ -1,4 +1,19 @@
 class profile::blog {
-
-
+  class { 'apache': 
+    docroot => '/var/www',  
+  }
+  class { 'apache::mod::php': }
+  
+  class { 'mysql::server':
+    root_password => 'strongpassword',
+  }
+  
+  class { 'mysql::bindings':
+    php_enable => true,
+  }
+  
+  class { 'wordpress':
+    install_dir => '/var/www',
+  }
+  
 }
