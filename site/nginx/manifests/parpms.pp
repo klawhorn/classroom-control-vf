6a640bc1c5fa83513bf8583d6d1ee2aeb04d3047
bmmakh <beena.makhijani@icbc.com>
2017-07-27 14:03:41 -0700

    Create parpms.pp

---
+++ site/nginx/manifests/parpms.pp
@@ -0,0 +1,34 @@
+class nginx::parpms {
+case $facts['os']['family'] {
+    'redhat' , 'debian': {
+      $package  = 'nginx'
+      $service  = 'nginx'
+      $docroot  = '/var/www'
+      $confdir  = '/etc/nginx'
+      $blockdir = "${confdir}/conf.d"
+      $logdir   = '/var/log/nginx'
+      $owner    = 'root'
+      $group    = 'root'
+    }
+    'windows' : {
+      $package  = 'nginx'
+      $service  = 'nginx'
+      $docroot  = 'C:/ProgramData/nginx/html'
+      $confdir  = 'C:/ProgramData/nginx'
+      $blockdir = "${confdir}/conf.d"
+      $logdir   = "${confdir}/logs"
+      $owner    = 'Administrator'
+      $group    = 'Administrators'
+    }
+    default : {
+      fail("Module ${module_name} is not supported on ${facts['os']['family']}")
+    }
+  }
+
+  $user = $facts['os']['family'] ? {
+    'redhat'  => 'nginx',
+    'debian'  => 'www-data',
+    'windows' => 'nobody',
+    default   => 'nginx',
+}
+}
