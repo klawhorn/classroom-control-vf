765966590d74ab4932b046b9005ff36168b2c7f0
Katie Lawhorn <katie.lawhorn@puppet.com>
2017-07-27 13:54:17 -0700

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,3 +1,28 @@
 class nginx::params {
-
+  case $facts['os']['family'] {
+    'redhat' , 'debian': {
+      $package  = 'nginx'
+      $service  = 'nginx'
+      #$docroot  = '/var/www'
+      $defdocroot  = '/var/www'
+      $confdir  = '/etc/nginx'
+      $blockdir = "${confdir}/conf.d"
+      $logdir   = '/var/log/nginx'
+      $owner    = 'root'
+      $group    = 'root'
+    }
+    'windows' : {
+      $package  = 'nginx'
+      $service  = 'nginx'
+      #$docroot  = 'C:/ProgramData/nginx/html'
+      $defdocroot  = 'C:/ProgramData/nginx/html'
+      $confdir  = 'C:/ProgramData/nginx'
+      $blockdir = "${confdir}/conf.d"
+      $logdir   = "${confdir}/logs"
+      $owner    = 'Administrator'
+      $group    = 'Administrators'
+    }
+    default : {
+      fail("Module ${module_name} is not supported on ${facts['os']['family']}")
+    }
 }
