23c56c0310a023b4ce5cfae38a25c90463b66985
Katie Lawhorn <katie.lawhorn@puppet.com>
2017-07-27 14:09:39 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,13 +1,13 @@
 class nginx ( 
-      String $package  = nginx::params::package
-      String $service  = nginx::params::service
-      String $docroot  = nginx::params::docroot
-      String $confdir  = nginx::params::confdir
-      String $blockdir = nginx::params::blockdir
-      String $logdir   = nginx::params::logdir
-      String $owner    = nginx::params::owner
-      String $group    = nginx::params::group
-      String $user     = nginx::params::user
+      String $package  = nginx::params::package,
+      String $service  = nginx::params::service,
+      String $docroot  = nginx::params::docroot,
+      String $confdir  = nginx::params::confdir,
+      String $blockdir = nginx::params::blockdir,
+      String $logdir   = nginx::params::logdir,
+      String $owner    = nginx::params::owner,
+      String $group    = nginx::params::group,
+      String $user     = nginx::params::user,
   ) inherits nginx::params {
   
   package { $package:
