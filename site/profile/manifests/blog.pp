db60f93abc8a6056ac92ae72b4a275fdc4737da7
Katie Lawhorn <katie.lawhorn@puppet.com>
2017-07-27 15:57:56 -0700

    Update blog.pp

--- site/profile/manifests/blog.pp
+++ site/profile/manifests/blog.pp
@@ -1,4 +1,18 @@
 class profile::blog {
-  class { 'apache': }
-  class {'apache::mod::php': }
+  class { 'apache': 
+    docroot => '/var/www',
+  }
+  class { 'apache::mod::php': }
+  
+  class { 'mysql::server': 
+    root_password => strongpassword,
+  }
+  
+  class { 'mysql::bindings': 
+    php_enable => true,
+  }
+  
+  class { 'wordpress': 
+    install_dir => '/var/www/',
+   }
 }
