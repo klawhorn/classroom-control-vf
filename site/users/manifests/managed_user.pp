925656a4d46810c0678bd429f784076f694d0507
Tara Swenson <kavi.xiu@gmail.com>
2017-07-27 11:13:44 -0700

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,25 +1,26 @@
+
 define users::managed_user (
   $group = $title,
 ){
   user { $title:
     ensure => present,
   }
-
+  
   $home = $facts['os']['family'] ? {
     'windows' => 'C:/Users',
-    default   =>  '/home',
+    default   => '/home',
   }
 
   file { "${home}/${title}":
-    ensure  =>  directory,
-    owner =>  $title,
-    group =>  $title,
-  } 
-
-  file "${home}/${title}/.ssh":
-    ensure  =>  directory,
-    owner =>  $title,
-    group =>  $title,
-    mode  =>  '0700',
-  } 
+    ensure => directory,
+    owner  => $title,
+    group  => $title,
+  }
+  
+  file { "${home}/${title}/.ssh":
+    ensure => directory,
+    owner  => $title,
+    group  => $title,
+    mode   => '0700',
+  }
 }
